976eff63ea4dfeaed90e76529eb709d2853585df
Jason Logerquist <jasonlogerquist@pop-os.localdomain>
2020-02-20 22:08:04 -0600

    start dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+set -g theme_display_date no
+set -xg EDITOR nvim
+
+eval (python -m virtualfish)
+
+set -g theme_nerd_fonts yes
+set -g fish_prompt_pwd_dir_length 2
+set -g ENHANCD_DOT_ARG '...'
+
+source "$HOME/.homesick/repos/homeshick/homeshick.fish"
